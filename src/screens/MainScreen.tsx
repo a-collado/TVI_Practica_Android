f6760ceaaa68ca238f878980de8c6de285562812
Anna Ferrer Giménez <anna.fg@students.salle.url.edu>
2023-11-01 23:59:02 +0100

    main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,10 +1,34 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import {View, Text, FlatList} from 'react-native';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
 import Input from '../components/Input/Input';
 import MusicVideoComponent from '../components/MusicVideoComponent/MusicVideoComponent';
 import {useFetch} from '../Api/Api';
+import { Touchable } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+  flexcolumn: {
+    flexDirection: 'column',
+    alignSelf: 'center', 
+    alignItems: 'center'
+  },
+  flexrow: {
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center', 
+    alignItems: 'center'
+  },
+  searchbutton: {
+    padding: 14, 
+    backgroundColor:'lightgray', 
+    borderRadius: 30, 
+    alignSelf: 'center',
+    borderWidth: 1,
+    width: 75
+  },
+});
 
 export default function MainScreen({navigation}) {
   const {search, data} = useFetch();
@@ -14,8 +38,19 @@ export default function MainScreen({navigation}) {
   };
 
   return (
-    <View>
-      <Input onSubmitEditing={onSearch} />
+    //<View style = {{backgroundColor: 'lightblue'}}>
+    <View style = {[styles.flexcolumn, {width: '90%'}]}>
+
+      <Text style = {{fontSize: 33, margin: 30}}>LS Search</Text>
+
+      <View style={{flexDirection: 'row', alignSelf: 'center', justifyContent:'space-around'}}>
+        <Input onSubmitEditing = {onSearch} />
+
+        <TouchableOpacity style={[styles.searchbutton, {marginLeft: 15}]}>
+          <Text style = {{color: 'white'}}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+
       {data && (
         <View style={{marginTop: 16}}>
           <FlatList
@@ -39,5 +74,6 @@ export default function MainScreen({navigation}) {
         </View>
       )}
     </View>
+    //</View>
   );
 }
